275e1a9dfa083b9cc8f4d402b2c6d0804186d97c
Lucas Bonomo <bonomo.lucas@gmail.com>
2020-12-31 12:42:36 -0300

    Init project

--- src/clockify.js
+++ src/clockify.js
@@ -5,3 +5,5 @@ const api_key = process.env.api_key
 
 console.log(api_url)
 console.log(api_key)
+
+console.log(`curl -H "content-type: application/json" -H "X-Api-Key: ${api_key}" -X GET ${api_url}/user`
